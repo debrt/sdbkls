ae2bbdb68de6a01b910da054619be0efc0ed23d3
JonathanKnopka <j.gamer1000@gmail.com>
2018-02-23 11:24:38 +0100

    updating the GUI.java

--- GUI.java
+++ GUI.java
@@ -4,8 +4,35 @@ import javax.swing.*;
 
 public class GUI
 {
+    JFrame frame;
+    JTabbedPane tabs;
+    JPanel tab1;
+    JPanel tab1N;
+    JPanel tab1S;
+    JPanel tab1W;
+    JPanel tab1E;
+
     public GUI()
     {
+        frame = new JFrame("Datenbank");
+        tabs = new JTabbedPane();
+        tab1 = new JPanel();
+        tab1N = new JPanel();
+        tab1S = new JPanel();
+        tab1W = new JPanel();
+        tab1E = new JPanel();
+
+        frame.setSize(600, 800);
+        frame.setVisible(true);
+
+        tabs.addTab("Übersicht", tab1);
+        frame.add(tabs);
+        tab1.setLayout(new BorderLayout());
+        tab1.add(tab1N, BorderLayout.NORTH);
+        tab1.add(tab1S, BorderLayout.SOUTH);
+        tab1.add(tab1W, BorderLayout.WEST);
+        tab1.add(tab1E, BorderLayout.EAST);
+
         
     }
 }
