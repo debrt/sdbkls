4dec9abf72c44cc78605f915462c0f97e0008e29
JonathanKnopka <j.gamer1000@gmail.com>
2018-03-02 09:25:04 +0100

    updated the MainWindow and added a table as well as the possibilty to scroll through it.

--- MainWindow.java
+++ MainWindow.java
@@ -3,8 +3,11 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+
+
  
-public class mainWindow
+public class MainWindow
 {
     JFrame frame;
     JTabbedPane tabs;
@@ -14,16 +17,20 @@ public class mainWindow
     JPanel tab1S;
     JPanel tab1W;
     JPanel tab1E;
+    JPanel tab1C;
+    JTable table;
     
     JTextField suche;
     
 	public static void main(String[] args){
-		mainWindow mainWindow = new mainWindow();
+		MainWindow mainWindow = new MainWindow();
 	}
 	
 
-    public mainWindow()
+    public MainWindow()
     {
+    	DefaultTableModel model = new DefaultTableModel(new String[] {"Test1", "Test2"}, 0 );
+    	table = new JTable(model);
         frame = new JFrame("SchülerDatenbank");
         tabs = new JTabbedPane();
         tab1 = new JPanel();
@@ -32,6 +39,9 @@ public class mainWindow
         tab1S = new JPanel();
         tab1W = new JPanel();
         tab1E = new JPanel();
+        tab1C = new JPanel();
+        
+        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE ); frame.pack(); frame.setVisible( true );
         
         suche = new JTextField("Suchbegriff...");
         suche.setSize(600,300);
@@ -47,8 +57,14 @@ public class mainWindow
         tab1.add(tab1S, BorderLayout.SOUTH);
         tab1.add(tab1W, BorderLayout.WEST);
         tab1.add(tab1E, BorderLayout.EAST);
+        tab1.add(tab1C, BorderLayout.CENTER);
         
         tab1N.add(suche);
+        //tab1C.add(new JTextField("test"));
+        tab1C.setLayout(new BorderLayout());
+        tab1C.add(new JScrollPane(table), BorderLayout.CENTER);
+        
+        
 
         
     }
