c438d62a7e14e227d04d5d4f9bd81870d1aba289
VincentKLS <Vincent@PC-Vincent>
2018-03-02 10:46:42 +0100

    Vincents Zeug

--- AddWindow.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.JFrame;
-
-public class AddWindow extends JFrame {
-	private static final long serialVersionUID = 7094360393888216438L;
-
-	public AddWindow() {
-		super("Schüler hinzufügen");
-	}
-}
