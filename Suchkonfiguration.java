db5b6c74ea853169b202e037b7c0460ce65be2a1
VincentKLS <Vincent@PC-Vincent>
2018-03-06 09:34:53 +0100

    Suchkonfiguration erste Design-Fundamente

--- Suchkonfiguration.java
+++ Suchkonfiguration.java
@@ -1,20 +1,39 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.sql.ResultSet;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 
-
 public class Suchkonfiguration{
    
+	static JPanel konfiguration;
+	static JLabel titelLabel;
+	static JLabel klsLogoLabel;
+	static JLabel benutzerBeschriftung;
+	static JLabel pwBeschriftung;
+	static JTextField benutzerField;
+	static JTextField pwField;
+	
     public Suchkonfiguration(){
     }
     
     public static JPanel getPanel(){
     	
-    	JPanel konfiguration = new JPanel();
+    	konfiguration = new JPanel();
     	konfiguration.setLayout(new FlowLayout());
+    	konfiguration.setLayout(new BoxLayout(konfiguration, BoxLayout.Y_AXIS));
+		konfiguration.setBackground(new Color(1, 68, 131));
+    	
+    	addTitelLabel();
+    	addImg();
+    	addFieldBeschriftungen();
+    	addBenutzerField();
+    	addPwField();
+    	
     	String[] suchkriterien = {};
     	try{
     		ResultSet rset = DBHelper.st.executeQuery(
@@ -35,12 +54,52 @@ public class Suchkonfiguration{
     	
     	for(int i=0; i<suchkriterien.length; i++) {
     		JCheckBox cb = new JCheckBox(suchkriterien[i]);
+    		konfiguration.add(titelLabel);
+    		konfiguration.add(klsLogoLabel);
+    		konfiguration.add(benutzerBeschriftung);
+    		konfiguration.add(benutzerField);
+    		konfiguration.add(pwBeschriftung);
+    		konfiguration.add(pwField);
     		konfiguration.add(cb);
     	}
     	
     	return(konfiguration);
     }
-
+	
+	private static void addTitelLabel(){
+		titelLabel = new JLabel("<html><body>"
+				+ "KöNIGIN-LUISE-STIFTUNG BERLIN <br> SCHULDATENBANK"
+				+ "</html></body>");
+		titelLabel.setForeground(Color.WHITE);
+	}
+	
+	private static void addImg(){
+		klsLogoLabel = new JLabel();
+		try{
+			BufferedImage klsLogo = ImageIO.read(new File("kls_logo.png"));
+			klsLogoLabel = new JLabel(new ImageIcon(klsLogo));
+		} catch(Exception e) {System.out.println("Fehler beim Laden des Bildes: " + e);}
+		if (klsLogoLabel == null){
+			klsLogoLabel = new JLabel("KLS-Logo");
+		}
+	}
+	
+	private static void addFieldBeschriftungen(){
+		benutzerBeschriftung = new JLabel("<html><body><i>BENUTZER       </i></body></html>");
+		benutzerBeschriftung.setForeground(Color.ORANGE);
+		pwBeschriftung = new JLabel("<html><body><i>PASSWORT</i></body></html>");
+		pwBeschriftung.setForeground(Color.ORANGE);
+	}
+	
+	private static void addBenutzerField(){
+		benutzerField = new JTextField(9);
+		benutzerField.setToolTipText("NUTZER");
+	}
+	
+	private static void addPwField(){
+	pwField = new JPasswordField(9);
+	pwField.setToolTipText("PASSWORT");
+}
     
     
 }
