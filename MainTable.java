cce0c9106dac9f73a8af6e0b8c66cda63ade7ef3
JonathanKnopka <j.gamer1000@gmail.com>
2018-03-09 11:10:53 +0100

    mehr Spalten

--- MainTable.java
+++ MainTable.java
@@ -1,9 +1,10 @@
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
 
 public class MainTable {
 
-	static String[] attribute = {"Klasse", "Name", "Vorname", "Geburtsdatum"};
+	static String[] attribute = {"Klasse", "Name", "Vorname", "Geburtsdatum","Hallo1","Hallo2","Hallo3","Hallo4","Hallo5"};
 	static DefaultTableModel model = new DefaultTableModel(attribute, 0 );
 	
 	public static DefaultTableModel getTable(){
@@ -15,10 +16,19 @@ public class MainTable {
 		for(int i = 0; i < model.getRowCount(); i++){
 			model.removeRow(i);
 		}
+		JTable table = Startansicht.getTable();
+		for(int i = 0; i < model.getColumnCount(); i++){
+			//table.getColumnModel().removeColumn(table.getColumnModel().getColumn(i));
+		}
+		TableColumn c = new TableColumn();
+		c.setHeaderValue("Klasse");
+		//table.getColumnModel().addColumn(c);
+		
+		//model = new DefaultTableModel(attribute, 0 );
+		
 		for(int i = 0; i < 50; i++){
 			model.addRow(new String[]{"Q4", "Mustermann", "Max", "20.01.1999"});
 			model.addRow(new String[]{"10A", "Beispiel","Bernd","01.01.2000"});
 		}
-		
 	}
 }
