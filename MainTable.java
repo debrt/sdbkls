d3729d49b2b86369fd2febe0368975dc6f226b35
JonathanKnopka <j.gamer1000@gmail.com>
2018-03-09 10:49:49 +0100

    reset der Tabelle bei Verwendung der Suche

--- MainTable.java
+++ MainTable.java
@@ -2,19 +2,23 @@ import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
 public class MainTable {
+
+	static String[] attribute = {"Klasse", "Name", "Vorname", "Geburtsdatum"};
+	static DefaultTableModel model = new DefaultTableModel(attribute, 0 );
 	
 	public static DefaultTableModel getTable(){
-		String[] attribute = {"Klasse", "Name", "Vorname", "Geburtsdatum"};
-		DefaultTableModel model = new DefaultTableModel(attribute, 0 );
-		for(int i = 0; i < 50; i++){
-			model.addRow(getRow());
-			model.addRow(new String[]{"10A", "Bernd","Beispiel","01.01.2000"});
-		}
+		refreshTable();
 		return model;
 	}
 	
-	static String[] getRow(){
-		String[] newRow = {"Q4", "Mustermann", "Max", "20.01.1999"};
-		return newRow;
+	public static void refreshTable( ) {
+		for(int i = 0; i < model.getRowCount(); i++){
+			model.removeRow(i);
+		}
+		for(int i = 0; i < 50; i++){
+			model.addRow(new String[]{"Q4", "Mustermann", "Max", "20.01.1999"});
+			model.addRow(new String[]{"10A", "Beispiel","Bernd","01.01.2000"});
+		}
+		
 	}
 }
