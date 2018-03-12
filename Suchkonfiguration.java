8db7e2d1a9e85b1198d489c15226a755be48031a
VincentKLS <Vincent@PC-Vincent>
2018-03-12 17:59:42 +0100

    Kommentieren des Codes, graphische Optimierungen

--- Suchkonfiguration.java
+++ Suchkonfiguration.java
@@ -16,6 +16,7 @@ import javax.swing.JTable;
 
 public class Suchkonfiguration{
    
+	//Initialisierung der Variablen
 	static JPanel konfiguration;
 	static JPanel tab1N;
     static JPanel tab1S;
@@ -32,76 +33,99 @@ public class Suchkonfiguration{
     	titelZeile();
     	applyButton();
     	presets();
+    	
     	konfiguration = new JPanel();
+    	
+    	//Hinzufügen der Komponenten zum Hauptpanel bzw. zum Fenster
     	konfiguration.setLayout(new BorderLayout());
 		konfiguration.setBackground(new Color(1, 68, 131));
     	konfiguration.add(tab1N, BorderLayout.NORTH);
     	konfiguration.add(tab1C, BorderLayout.CENTER);
     	konfiguration.add(tab1W, BorderLayout.WEST);
     	konfiguration.add(tab1E, BorderLayout.EAST);
+    	//Rückgabe des erstellten Panels an MainWindow, wo dieses als Tab hinzugefügt wird
     	return(konfiguration);
     }
 
     static void presets(){
+    	//Setup des linken Seit-Panels
     	tab1W = new JPanel();
     	tab1W.setLayout(new BoxLayout(tab1W, BoxLayout.Y_AXIS));
 		tab1W.setBackground(new Color(1,68,131));
-		JLabel presetLabel = new JLabel("Datensatz Preset: ");
+		//Setup der Auswahl von Presets (vordefinierten Ansichtseinstellungen)
+		JLabel presetLabel = new JLabel("Preset: ");
         presetLabel.setForeground(Color.WHITE);
-        String[] presets = {"Schüler*innen", "+Erziehungsberechtigte"};
+        String[] presets = {"Schüler*innen", "Erziehungsberechtigte", "Klassendaten"};
         JComboBox<String> presetAuswahl= new JComboBox<String>(presets);
         presetAuswahl.setMaximumSize(new Dimension(150,28));
+        //Hinzufügen der Preset-Auswahl zum linken Seit-Panel
         tab1W.add(presetLabel);
 		tab1W.add(presetAuswahl);
     }
     
 	static void auswahl(){
+		//Setup des mittleren Haupt-Panels
 		tab1C = new JPanel();
 		tab1C.setBackground(new Color(1,68,131));
 		tab1C.setLayout(new BoxLayout(tab1C, BoxLayout.Y_AXIS));
+		//Auslesen der verfügbaren Attribute aus der Startansichtstabelle
+		//(dies sollte später aus der Datenbank ausgelesen werden)
 		try {
 			for(int i=0; i < Startansicht.getTable().getColumnCount(); i++) {
+				//Jedes Attribut wird als String in einem Array gespeichert
 				suchkriterien.add(Startansicht.getTable().getColumnName(i));
 			}
 		} catch (Exception e){
 			e.printStackTrace();
 		}
+		//Für jeden String im Array suchkriterien wird diese Schleife einmal ausgeführt
     	for(int i=0; i<suchkriterien.size(); i++) {
+    		//Setup eines neuen Panels
     		JPanel pa = new JPanel();
     		pa.setBackground(new Color(1,68,131));
+    		//(Ist notwendig, um eine mittige Ausrichtung der Komponenten einzustellen)
     		pa.setLayout(new FlowLayout(FlowLayout.CENTER));
+    		//Hinzfügen einer Checkbox für jedes Attribut
     		JCheckBox cb = new JCheckBox(suchkriterien.get(i));
     		cb.setBackground(new Color(1,68,131));
     		cb.setForeground(Color.WHITE);
     		cb.setSelected(true);
+    		//Hinzufügen der Checkboxen zum Panel
     		pa.add(cb);
+    		//Hinzufügen des jeweiligen Panels zum Mitte-Panel
     		tab1C.add(pa);
+    		//Speichern der Checkboxen in einem Array (später zur Auslese benötigt)
     		checkboxen.add(cb);
     	}
 	}
 	
 	static void titelZeile(){
+		//Setup des obigen Titel/Überschriften-Panels
 		tab1N = new JPanel();
         tab1N.setBackground(new Color(1,68,131));
+        //Hinzufügen von Überschrift und Logo aus dem LogIn-Window (Code-Ersparnis)
         tab1N.add(LogInWindow.titelPanel);
 	}
 	static void applyButton(){
+		//Setup des rechtsseitigen Panels
 		tab1E = new JPanel();
         tab1E.setBackground(new Color(1,68,131));
+        //Hinzufügen eines Knopfes zum übernehmen der Auswahl
         JButton filtern = new JButton("Filter übernehmen");
+        //Setup & HInzufügen der Funktionalität dieses Knopfes
         filtern.addActionListener(new ActionListener() {
         	public void actionPerformed(ActionEvent e) {
-        		
+        		//Für jede Checkbox wird nachfolgender Code einmal ausgeführt
         		for (int i=0; i < checkboxen.size(); i++) {
+        			//Ist eine Checkbox deaktiviert wird folgender Code ausgeführt
         			if(!checkboxen.get(i).isSelected()) {
+        				//Das Attribut bzw. die Spalte an Stelle i wird aus der Tabelle entfernt
         				Startansicht.getTable().removeColumn(Startansicht.getTable().getColumnModel().getColumn(i));
-        			}
-        			else {
-        				//Startansicht.getTable().addColumn(Startansicht.getTable().getColumnModel().getColumn(i));
         			}
         		}
         	}
         });
+        //Hinzufügen des Knopfes zum rechtsseitigen Panel 
     	tab1E.add(filtern);
 	}    
 }
